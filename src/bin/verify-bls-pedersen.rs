4157c8939233d0cdcaf36842bcc058c6e9b0fa2e
flyq <flyq951@gmail.com>
2024-01-27 19:14:15 +0800

    .

--- src/bin/verify-bls-pedersen.rs
+++ src/bin/verify-bls-pedersen.rs
@@ -1,15 +1,25 @@
-use bls_pedersen::bls::verify;
 use bls_pedersen::data::puzzle_data;
 use bls_pedersen::PUZZLE_DESCRIPTION;
+use bls_pedersen::{bls::verify, hash};
 use prompt::{puzzle, welcome};
 
 fn main() {
     welcome();
     puzzle(PUZZLE_DESCRIPTION);
+    println!("{:?}", u8_to_bits(&[5, 15]));
     let (pk, ms, sigs) = puzzle_data();
     for (m, sig) in ms.iter().zip(sigs.iter()) {
         verify(pk, m, *sig);
     }
+
+    // let a = matrix![2., 3.;
+    //             1., 2.];
+    // let inv = a
+    //     .clone()
+    //     .inverse()
+    //     .expect("This matrix should have an inverse!");
+
+    // let I = a * inv;
 
     /* Your solution here! */
     /*
@@ -17,4 +27,25 @@ fn main() {
       let m = your username;
       verify(pk, m, sig);
     */
+
+    let h_m: Vec<Vec<u8>> = ms
+        .iter()
+        .map(|m| hash::hash_to_curve(m).0)
+        // .map(|i| u8_to_bits(&i))
+        .collect();
+
+    // let sig =
+    // let m = b"Rob".to_vec();
+}
+
+fn u8_to_bits(inputs: &[u8]) -> Vec<u8> {
+    let mut bits = Vec::with_capacity(inputs.len() * 8);
+
+    for input in inputs {
+        for i in 0..8 {
+            let bit = (input >> i) & 1;
+            bits.push(bit);
+        }
+    }
+    bits
 }
